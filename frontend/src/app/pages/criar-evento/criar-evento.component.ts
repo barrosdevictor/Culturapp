7ee2cec47811bfea2ad0e53f7f6e835026349086
Crislaine Barbosa <crislainebarbosa.barbosa993@gmail.com>
2025-06-08 19:53:31 -0300

    feat: inclusion of the form group in the edit-event and create-event files

--- frontend/src/app/pages/criar-evento/criar-evento.component.ts
+++ frontend/src/app/pages/criar-evento/criar-evento.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-evento',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterModule],
   templateUrl: './criar-evento.component.html',
   styleUrls: ['./criar-evento.component.css']
 })
-export class CriarEventoComponent {
-  isEditing = false;
+export class CriarEventoComponent  {
+  eventoForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.eventoForm = this.fb.group({
+      titulo: [''],
+      tipo: [''],
+      descricao: [''],
+      capacidade: [''],
+      valorIngresso: [''],
+      pontos: [''],
+      cep: [''],
+      numero: [''],
+      estado: [''],
+      cidade: [''],
+      rua: [''],
+      bairro: [''],
+      complemento: [''],
+      dataInicioEvento: [''],
+      dataFimEvento: [''],
+      inicioVenda: [''],
+      fimVenda: [''],
+      status: ['ATIVO']
+    });
+  }
+
+  salvarEvento(): void {
+    if (this.eventoForm.valid) {
+      console.log(this.eventoForm.value);
+      // aqui você pode enviar os dados para a API
+    }
+  }
 }
